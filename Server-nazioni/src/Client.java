ad7c87d82ff5cf5019581a8036d5ab7253847fc2
riggio05 <simone.riggio@itiszuccante.edu.it>
2023-03-09 12:02:23 +0100

    nazioni

--- Server-nazioni/src/Client.java
+++ Server-nazioni/src/Client.java
@@ -1,16 +1,26 @@
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
 
 public class Client {
-
-    public static void main(String[] args) {
-
-        msg.read
-        DatagramSocket s = new DatagramSocket(porta);
-        byte[] buffer1 = msg.getBytes();
-        InetAddress hostAddress = InetAddress.getByName("localhost");
-        DatagramPacket paket = new DatagramPacket(buffer, buffer1.length);
-
+    public static void main(String[] args) throws IOException {
+        //socket e buffer
+        DatagramSocket sk = new DatagramSocket();
+        byte[] buffer = new byte[65536];
+        String msg = "";
+        Scanner input = new Scanner(System.in);
+        System.out.print("Di quale nazione vuoi sapere la capitale? ");
+        msg = input.nextLine(); //input nazione
+        DatagramPacket packet =
+        new DatagramPacket(buffer, buffer.length, InetAddress.getLocalHost(), 8080); //pacchetto
+        packet.setData(msg.getBytes()); //riempio il pacchetto con i dati
+        sk.send(packet);
+        byte[] bufferRisposta = new byte[65536]; //buffer
+        DatagramPacket packetRisposta = new DatagramPacket(bufferRisposta, bufferRisposta.length);
+        sk.receive(packetRisposta);
+        byte[] data = packetRisposta.getData();
+        int len = packetRisposta.getLength();
+        String risposta = new String(data, 0, len);
+        System.out.println("La capitale di " + msg + " è: " + risposta);
     }
 }
--- Server-nazioni/src/Server.java
+++ Server-nazioni/src/Server.java
@@ -1,2 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Server {
+    public static void main(String[] args) throws IOException {
+        //socket e buffer
+        DatagramSocket sk = new DatagramSocket(8080);
+        byte[] buffer = new byte[65536];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        sk.receive(packet);
+        byte[] data = packet.getData();
+        int len = packet.getLength();
+        String msg = new String(data, 0, len);
+        int port = packet.getPort(); //porta mittente
+        InetAddress address = packet.getAddress(); //indirizzo mittente
+        File file = new File("src/NazioniCapitali.csv");
+        String risposta = "";
+        byte[] bufferRisposta = risposta.getBytes();
+        DatagramPacket packetRisposta = new DatagramPacket(bufferRisposta, bufferRisposta.length, address, port);
+        sk.send(packetRisposta);
+        sk.close();
+    }
 }
